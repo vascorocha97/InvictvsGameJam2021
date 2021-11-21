cb205e9b5f007ca9937be484292e79968bb8c1a5
Vasco <1161216@isep.ipp.pt>
2021-11-21 00:00:41 +0000

    Dialogue System Finalized(?)

--- Assets/_Scripts/NPC.cs
+++ Assets/_Scripts/NPC.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class NPC : MonoBehaviour
 {
     public GameObject dialogueBox;
+    private int count = 0;
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject == Player.Instance.gameObject)
+        if (collision.gameObject == Player.Instance.gameObject)
+        {           
             dialogueBox.SetActive(true);
+            if (count > 0)
+            {
+                Dialogue.Instance.StartDialogue();
+            }
+            count++;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.gameObject == Player.Instance.gameObject)
+        if (collision.gameObject == Player.Instance.gameObject)
+        {
             dialogueBox.SetActive(false);
+        }
     }
+
 }
