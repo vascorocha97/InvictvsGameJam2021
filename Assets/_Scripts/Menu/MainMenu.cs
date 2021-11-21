d9be0395e78509469961a2ec0043e9fc1b1afeef
João Ribeiro <joao--ribeiro@hotmail.com>
2021-11-21 02:48:26 +0000

    Menu

---
+++ Assets/_Scripts/Menu/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+
+    public void palyGame()
+    {
+        // TODO load lvl 1 !!!
+        // SceneManager.LoadScene();
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
